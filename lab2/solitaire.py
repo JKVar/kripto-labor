8ae2ad78c87265a171d3e824ec1d9c51f388e20f
JKVar <61911386+JKVar@users.noreply.github.com>
2022-12-02 16:13:09 +0200

    blum-blum-shub elkezdve

--- lab2/solitaire.py
+++ lab2/solitaire.py
@@ -1,3 +1,6 @@
+import random
+from sympy import nextprime
+
 # solitaire algorithm
 # white joker = 53, balck joker = -53 <- csak a megkulomboztetes kedveert
 
@@ -50,12 +53,62 @@ def switch(cards, joker1, joker2):
 
     return new_order
 
+# d.
+# az utolso kartyalapot megnezzuk, majd elolrol annyit rakunk az utolso kartyalap ele, amennyi az erteke
+def last_card(cards):
+    nn = len(cards)
+    value = abs(cards[nn-1])
+    new_cards = cards[value:(nn-1)] + cards[0:value] + cards[(nn-1):nn]
+
+    return new_cards
+
+# e.
+def key_member(cards):
+    value = abs(cards[0])
+    if value == 53:
+        return -1
+    else:
+        return abs(cards[value])
+
 def solitaire(cards, n):
     i = 0
+    key = []
     while i < n:
         white_joker_index = white_joker(cards)
         black_joker_index = black_joker(cards)
         cards = switch(cards, white_joker_index, black_joker_index)
+        cards = last_card(cards)
 
-        i += 1
+        keym = key_member(cards)
+        if keym != -1:
+            i += 1
+            key.append(keym)
 
+    return key
+
+def blum_blum_shub(seed, n):
+    p = nextprime(4568456)
+    while p%4 != 3:
+        p = nextprime(p)
+
+    q = nextprime(4569000)
+    while q%4 != 3:
+        q = nextprime(q)
+
+    M = p*q
+    key = bytearray(b'')
+    x = (seed**2)%M
+    for i in range(n):
+        st = ''
+        for j in range(8):
+            st += str((x**2)%M%2)
+            x = (x**2)%M
+        key.append(chr(int(st, 2)).encode())
+
+    return key
+
+def main():
+    n = 200
+    cards = [42, 46, 26, 28, 38, 16, 19, 20, 9, 53, 37, 33, 35, -53, 1, 7, 13, 41, 34, 32, 11, 48, 40, 29, 25, 50, 45, 5, 44, 49, 36, 8, 43, 51, 2, 14, 18, 27, 12, 21, 17, 23, 4, 30, 24, 6, 39, 52, 47, 22, 10, 15, 3, 31]
+    key = solitaire(cards, n)
+    print(key)
--- lab2/streamen_cryption.py
+++ lab2/streamen_cryption.py
@@ -1,4 +1,23 @@
-def stream_encryption(message, key, key_generator: function):
-    # i need to expand the key with the key_generator algorithm
-    # then xor the message with the expanded key
-    print(message)
+import solitaire as keygen
+
+def xor(barr1, barr2):
+    new_barr = bytearray(b'')
+    for a, b in zip(barr1, barr2):
+        new_barr.append(a^b)
+
+    return new_barr
+
+def stream_encryption(message, key, key_generator):
+    array_len = len(message)
+
+    key = key_generator(key, array_len)
+    print(len(key))
+    encoded_message = xor(message, key)
+    print(encoded_message)
+
+    return encoded_message
+
+st = 'Hello'
+st_bin = bytearray(st, 'ascii')
+print(st_bin)
+encoded_message = stream_encryption(st_bin, 23, keygen.blum_blum_shub)
