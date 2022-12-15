39c933999faf3a503030b1301299e20ec870ad34
JKVar <vargajozsikristof11@gmail.com>
2022-12-15 18:11:40 +0200

    Markle-Hellman decryption done

--- lab3/merkle_hellman.py
+++ lab3/merkle_hellman.py
@@ -1,6 +1,7 @@
 import random
 import utils
 
+# returns the generated superincreasing sequence and the total sum of it
 def generate_superincreasing_sequence(n):
     sequence = [random.randint(2,10)]
     total = sequence[0]
@@ -12,22 +13,23 @@ def generate_superincreasing_sequence(n):
 
     return sequence, total
 
+# return the coprime of a and b
 def coprime(a, b):
-    while b != 0:
-        c = b
-        b = a % b
-        a = c
+    while b:
+        a, b = b, a % b
 
     if a == 1:
         return True
+
     return False
 
-def keygen_merkle_hellmann(n = 8):
+# generates the public and private key with Merkle-Hellman algorithm
+def keygen_merkle_hellman(n = 8):
     w, total = generate_superincreasing_sequence(n)
     q = random.randint(total+1, 2*total)
 
     r = random.randint(2, q-1)
-    while coprime(r, q):
+    while not coprime(r, q):
         r = random.randint(2, q-1)
 
     beta = tuple(map(lambda b: (r*b)%q, w))
@@ -35,32 +37,52 @@ def keygen_merkle_hellmann(n = 8):
 
     return beta, (w,q,r)
 
-def char_to_binary_list(char):
-    num = ord(char)
-    return list(map(lambda k: int(k), bin(num)[2:]))
-
-def encrypt_merkle_hellmann(plain_text, key: tuple):
+# encrypts the message with Merkle-Hellman
+def encrypt_merkle_hellman(plain_text, key: tuple):
     cipher_text = ''
     for char in plain_text:
         binary_list = utils.byte_to_bits(ord(char))
-        # binary_list = char_to_binary_list(char)
         ci = sum(map(lambda x: x[0]*x[1], zip(binary_list, key)))
         cipher_text += chr(ci)
 
     return cipher_text
 
-def decrypt_merkle_hellmann(cipher_text, key: tuple):
-    raise NotImplementedError
+# solves the superincreasing subset sum 
+def solve_supinc_ss(w, cc):
+    indexes = []
+    key_length = len(w)
+    subset_sum = 0
+
+    for index, wi in enumerate(w[::-1]):
+        if wi <= cc:
+            subset_sum += wi
+            cc = cc - wi
+            indexes.append(key_length - index)
+
+    return indexes
+
+# decrypts the message with Merkle-Hellman
+def decrypt_merkle_hellman(cipher_text, key: tuple):
+    w, q, r = key
+    key_length = len(w)
+    s = utils.modinv(r,q) # modular inverse
+    decrypted_text = ''
+
+    for char in cipher_text:
+        cc = (ord(char)*s) % q
+        indexes = solve_supinc_ss(w, cc)
+        decrypted_text += chr(sum(map(lambda i: 2**(key_length-i), indexes)))
+
+    return decrypted_text
 
 def main():
-    public_key, private_key = keygen_merkle_hellmann()
-    print(public_key)
-    message = 'Hello'
-    print('-------------------')
-    secret_msg = encrypt_merkle_hellmann(message, public_key)
-    print('-------------------')
+    public_key, private_key = keygen_merkle_hellman()
+    message =  input(' [] Enter the message here:\n >> ')
+    secret_msg = encrypt_merkle_hellman(message, public_key)
+    decrypted_msg = decrypt_merkle_hellman(secret_msg, private_key)
 
-    print(secret_msg)
+    print('\n [] Encrypted message:\n >>', secret_msg)
+    print(' [] Decrypted message:\n >>', decrypted_msg)
 
 if __name__ == '__main__':
     main()
