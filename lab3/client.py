423477e6f6533390492de80e9c08dcfde57dc381
JKVar <vargajozsikristof11@gmail.com>
2022-12-21 01:00:47 +0200

    received data from keyserver

--- lab3/client.py
+++ lab3/client.py
@@ -1,6 +1,9 @@
 # this is the client
 import sys
+import socket
+import merkle_hellman as mh
 
+host = '0.0.0.0'
 server_port = 9000
 help_msg = '''
 python client.py [port]
@@ -8,9 +11,76 @@ python client.py [port]
     ex: python client.py 9001
 '''
 
+
+def regist_to_keyserver(my_name, public_key):
+    ssocket = socket.socket()
+    ssocket.connect((host, server_port))
+    req = 'HENLO'
+    ssocket.send(req.encode())
+    recv_msg = ssocket.recv(1024).decode()
+
+    if recv_msg == 'HENLO':
+        msg = 'MY_NAME_IS ' + str(my_name)
+        ssocket.send(msg.encode())
+        recv_msg = ssocket.recv(1024).decode()
+
+        if recv_msg == 'WHAT ' + my_name:
+            transformed_key = str(public_key).strip('()')
+            ssocket.send(transformed_key.encode())
+            recv_msg = ssocket.recv(1024).decode()
+            ssocket.close()
+
+            return recv_msg == 'OK'
+    
+    ssocket.close()
+    return False
+
+def find_user_on_server(other_user):
+    ssocket = socket.socket()
+    ssocket.connect((host, server_port))
+    req = 'HENLO'
+    print(req)
+    ssocket.send(req.encode())
+    recv_msg = ssocket.recv(1024).decode()
+
+    if recv_msg == 'HENLO':
+        msg = 'FIND ' + str(other_user)
+        ssocket.send(msg.encode())
+        recv_msg = ssocket.recv(1024).decode()
+        recv_msg = recv_msg.split()
+        print('>>>>', recv_msg)
+
+        if recv_msg[0] == 'PWD':
+            public_key = tuple(map(lambda x: int(x.strip(',')),
+                                recv_msg[1:len(recv_msg)-1]))
+            print(public_key)
+            ssocket.close()
+
+            return public_key
+    
+    ssocket.close()
+    return None
+
 def run_client(port):
-    print('client_id: ', port)
-    print('server port: ', server_port)
+    # generating public and provate keys
+    public_key, private_key = mh.keygen_merkle_hellman()
+    registered = regist_to_keyserver(port, public_key)
+    
+    if not registered:
+        print('> registration failed')
+        return 
+
+    print('> successfull registration')
+
+    other_user = input('Who do you want to talk to?\n>>> ')
+    
+    key = find_user_on_server(other_user)
+
+    if not key:
+        print('> not found other user')
+        return 
+    
+    print('> found other user')
 
 # checking the arguments
 if len(sys.argv) == 1:
--- lab3/keyserver.py
+++ lab3/keyserver.py
@@ -1,1 +1,77 @@
 # this is the keyserver
+import socket
+import threading
+
+lock = threading.Lock()
+
+host = '0.0.0.0'
+server_port = 9000
+s = socket.socket()
+s.bind((host, server_port))
+s.listen(1)
+
+def register_user(client, user_name, user_list):
+    print('> registering user', user_name)
+    msg = 'WHAT ' + user_name
+    client.send(msg.encode())
+
+    pwd = client.recv(1024).decode()
+    lock.acquire()
+    user_list[user_name] = pwd
+    lock.release()
+    print('> saving', user_name + ',s public key')
+
+    ok_msg = 'OK'
+    client.send(ok_msg.encode())
+
+def find_user(client, user_name, user_list):
+    print('> searching user', user_name)
+
+    if user_name not in user_list:
+        msg = 'NOT ' + user_name
+        client.send(msg.encode())
+        print('>', user_name, 'not found')
+        return
+
+    lock.acquire()
+    public_key = 'PWD ' + user_list[user_name]
+    lock.release()
+    client.send(public_key.encode())
+    print('> user ', user_name, 'is found')
+
+def talk_with_client(client, user_list):
+    print('\nuser_list: ', user_list)
+    print('> talking with client')
+    msg = 'HENLO'.encode()
+    client.send(msg)
+
+    recv_msg = client.recv(1024).decode()
+    req = recv_msg.split()
+
+    if req[0] == 'MY_NAME_IS':
+        what = req[1]
+        register_user(client, what, user_list)
+        print('> registration done for ', what)
+    elif req[0] == 'FIND':
+        what = req[1]
+        find_user(client, what, user_list)
+        print('>', what, 'found')
+
+user_list = {'9001': '456, 778, 989'}
+i = 0
+while True:
+    client, client_addr = s.accept()
+    recv_msg = client.recv(1024)
+
+    if recv_msg == 'stop':
+        break
+
+    th = threading.Thread(target=talk_with_client, 
+                args=(client, user_list, ))
+
+    # th.daemon = True
+    th.start()
+    print('th ', i)
+    i += 1
+
+s.close()
